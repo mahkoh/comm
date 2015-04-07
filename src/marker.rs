a99591a793d52ce5f4780440edf8ce9a837ffe99
Julian Orth <ju.orth@gmail.com>
2015-04-07 19:53:14 +0200

    update rust

--- src/marker.rs
+++ src/marker.rs
@@ -1,9 +1,7 @@
-use std::marker::{PhantomFn};
-
 /// Types able to be transferred across thread boundaries.
 ///
 /// If possible you should implement `std::marker::Send` instead of this trait. This trait
 /// exists because `Send` cannot be implemented for `*mut T` and `*const T`.
-pub unsafe trait Sendable: PhantomFn<Self> { }
+pub unsafe trait Sendable { }
 
 unsafe impl<T: Send+?Sized> Sendable for T { }
